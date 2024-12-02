d7a01c1851ae31f5c3dc69653fce5be924d50e2a
zaid <zaidrod08@gmail.com>
2024-12-02 19:54:34 +0100

    Completo api rest

---
+++ rest/rest_client.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cliente REST</title>
+
+    <h2>Listar libros</h2>
+    <form method="GET" action="">
+        <label>ISBN (opcional):</label>
+        <input type="text" name="isbn" placeholder="12345678910">
+        <button type="submit">Listar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['isbn'])) {
+        $isbn = $_GET['isbn'];
+        $url = "http://localhost/entornoServidor/Unidad4/Biblioteca/rest/rest_server.php/libros/" . urlencode($isbn);
+    } else {
+        $url = "http://localhost/entornoServidor/Unidad4/Biblioteca/rest/rest_server.php/libros";
+    }
+
+    $response = file_get_contents($url);
+    $libros = json_decode($response, true);
+
+
+    if ($libros != null){
+        echo "<ul>";
+        foreach ($libros as $unLibro) {
+            echo "<li>Titulo: {$unLibro['titulo']}, Autor: {$unLibro['autor']}, Categoria: {$unLibro['categoria']}, ISBN: {$unLibro['isbn']}, Edición: {$unLibro['edicion']}, Promoción: {$unLibro['promocion']}, Fecha: {$unLibro['fecha']}, Portada: {$unLibro['portada']}</li>";
+        }
+        echo "</ul>";
+    }else{
+        echo "<p>Error: No se pudieron cargar los datos de los libros.</p>";
+    }
+
+
+    ?>
+
+
+
+
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ rest/rest_server.php
@@ -0,0 +1,59 @@
+<?php
+header("Content-Type: application/json");
+/* require_once '/../controller/libreriaController.php'; */
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+$pathInfo = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : null;
+$request = $pathInfo ? explode('/', trim($pathInfo, '/')) : [];
+
+
+// Función para retornar el total de libros.
+
+function retornarLibros($isbn = null)
+{
+    $xmlFile = "../model/libreria.xml";
+    //$controller = new LibreriaController();
+    $libros = simplexml_load_file(  $xmlFile);
+    $libroEncontrado = false;
+
+    foreach ($libros->libro as $unLibro) {
+        if ($isbn == null || (string) $unLibro->isbn === $isbn) {
+            $consulta[] = [
+                'titulo' => (string) $unLibro->titulo,
+                'autor' => (string) $unLibro->autor,
+                'categoria' => (string) $unLibro->categoria,
+                'isbn' => (string) $unLibro->isbn,
+                'edicion' => (string) $unLibro->edicion,
+                'promocion' => (string) $unLibro->promocion,
+                'fecha' => (string) $unLibro->fecha,
+                'portada' => (string) $unLibro->portada,
+            ];
+            
+        }
+    }
+
+
+    return $consulta;
+}
+
+//Procesar la solicitud.
+if ($method == 'GET') {
+
+    if (isset($request[0]) && $request[0] === 'libros') {
+        $isbn = isset($request[1]) ? $request[1] : null;
+        echo json_encode(retornarLibros($isbn));
+    } else {
+        http_response_code(404);
+        echo json_encode(["error" => "Recurso no encontrado"]);
+    }
+
+
+} else {
+    http_response_code(400);
+    echo json_encode(["error" => "Error con el método solicitado"]);
+}
+
+
+
+
