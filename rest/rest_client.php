21fa741ab0b391d2ab4a2eed3ed0d85261ee495f
zaid <zaidrod08@gmail.com>
2024-12-04 18:07:14 +0100

    Completados los cambios esteticos de SOAP y REST

--- rest/rest_client.php
+++ rest/rest_client.php
@@ -5,39 +5,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cliente REST</title>
+    <link rel="stylesheet" href="../view/style.css">
 
-    <h2>Listar libros</h2>
-    <form method="GET" action="">
-        <label>ISBN (opcional):</label>
-        <input type="text" name="isbn" placeholder="12345678910 ">
-        <button type="submit">Listar</button>
-    </form>
+    <?php echo "<div class='contenedor'>
+    <img  class='imagen' src='../view/icono.PNG'>
+    <p class='textoCab'> API REST </p> </div>"; ?>
+
+    <div class="contenedorCentrado">
+        <form method="GET" action="">
+            <label>ISBN (opcional):</label>
+            <input  type="text" name="isbn" placeholder="12345678910 ">
+            <button type="submit" name="listar " class="boton" value="1">Listar</button>
+        </form>
+    </div>
+
+
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['isbn'])) {
         $isbn = $_GET['isbn'];
         $url = "http://localhost/entornoServidor/Unidad4/Biblioteca/rest/rest_server.php/libros/" . urlencode($isbn);
-    } else {
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['listar'])) {
         $url = "http://localhost/entornoServidor/Unidad4/Biblioteca/rest/rest_server.php/libros";
+    } else {
+        echo "<div class='contenedorCentrado'> Bienvenido, puedes consultar el catalogo o buscar un libro por ISBN.</div>";
+        exit();
     }
+
 
     $response = file_get_contents($url);
     $libros = json_decode($response, true);
 
+    echo "<div class='contendorTabla'>";
+    if ($libros != null): ?>
+        <div class="titulosTabla">
+            <div class="col"> Titulo </div>
+            <div class="col"> Autor </div>
+            <div class="col"> Categoria </div>
+            <div class="col"> ISBN </div>
+            <div class="col"> Edición </div>
+            <div class="col"> Promoción </div>
+            <div class="col"> Fecha </div>
+            <div class="col"> Portada </div>
 
-    if ($libros != null){
-        echo "<ul>";
-        foreach ($libros as $unLibro) {
-            echo "<li>Titulo: {$unLibro['titulo']}, Autor: {$unLibro['autor']}, Categoria: {$unLibro['categoria']}, ISBN: {$unLibro['isbn']}, Edición: {$unLibro['edicion']}, Promoción: {$unLibro['promocion']}, Fecha: {$unLibro['fecha']}, Portada: {$unLibro['portada']}</li>";
-        }
-        echo "</ul>";
-    }else{
-        echo "<p>Error: No se pudieron cargar los datos de los libros.</p>";
-    }
+        </div>
+        <?php
 
-
+        foreach ($libros as $unLibro): ?>
+            <div class="listadoFila">
+                <div class="col"><?php echo $unLibro['titulo'] ?></div>
+                <div class="col"><?php echo $unLibro['autor'] ?></div>
+                <div class="col"><?php echo $unLibro['categoria'] ?></div>
+                <div class="col"><?php echo $unLibro['isbn'] ?></div>
+                <div class="col"><?php echo $unLibro['edicion'] ?></div>
+                <div class="col"><?php echo $unLibro['promocion'] ?></div>
+                <div class="col"><?php echo $unLibro['fecha'] ?></div>
+                <div class="col"><?php echo $unLibro['portada'] ?></div>
+            </div>
+        <?php endforeach; ?>
+    <?php else:
+        echo "<div class='contenedorCentrado'>Error: No se pudieron cargar los datos de los libros.</p>";
+    endif;
     ?>
-
+    </div>
 
 
 
--- soap/soap_client.php
+++ soap/soap_client.php
@@ -1,21 +1,132 @@
-<?php
-$options = ['location' => 'http://localhost/entornoServidor/Unidad4/Biblioteca/soap/soap_server.php', 'uri' => 'http://localhost/entornoServidor/Unidad4/Biblioteca/soap/'];
-$client = new SoapClient(null, $options);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API SOAP</title>
+    <link rel="stylesheet" href="../view/style.css">
+
+    <?php echo "<div class='contenedor'>
+<img  class='imagen' src='../view/icono.PNG'>
+<p class='textoCab'> API SOAP </p> </div>"; ?>
 
 
-// Listar empleados
-$libros = $client->listarLibros();
+    <!--  //echo $client->eliminarLibro("12345");
+    //echo $client->crearLibro("Milibro", "Yo","ciencia ficcion","12345","1","si","1994-06-29");
+ -->
+    <div class="titulo"> Bienvenido a la API SOAP de LibroSphere</div>
+</head>
 
-echo "<ul>";
-foreach ($libros as $unLibro) {
-    echo "<li>Titulo: {$unLibro['titulo']}, Autor: {$unLibro['autor']}, Categoria: {$unLibro['categoria']}, ISBN: {$unLibro['isbn']}, Edición: {$unLibro['edicion']}, Promoción: {$unLibro['promocion']}, Fecha: {$unLibro['fecha']}, Portada: {$unLibro['portada']}</li>";
-}
-echo "</ul>";
+<body>
 
-//echo $client->eliminarLibro("12345");
-//echo $client->crearLibro("Milibro", "Yo","ciencia ficcion","12345","1","si","1994-06-29");
+    <div class="contenedorEnlaces">
+        <!-- Formulario para creación de libros -->
+        <div class="contenedorCentrado">
+            <form method="POST" action="">
+                <div class="camposForm">
+                    <label> Titulo </label>
+                    <input required type="text" name="titulo">
+                    <label> Autor </label>
+                    <input required type="text" name="autor">
+                </div>
+                <div class="camposForm">
+                    <label> Categoria </label>
+                    <input required type="text" name="categoria">
+                    <label> ISBN </label>
+                    <input required type="text" name="isbn">
+
+                </div>
+                <div class="camposForm">
+                    <label> Edición </label>
+                    <input required type="text" name="edicion">
+                    <label> Promoción </label>
+                    <input required type="text" name="promocion">
+                </div>
+                <div class="camposForm">
+                    <label> Fecha </label>
+                    <input required type="date" name="fecha">
+                </div>
+                <button type="submit" class="boton" name="crear" value="1"> Crear registro </button>
+            </form>
+        </div>
+        <!-- Formulario para consulta de catalogo -->
+        <div class="contenedorCentrado">
+            <form method="GET" action="">
+                <button type="submit" class="boton" name="consultar" value="1"> Consultar catalogo </button>
+            </form>
+        </div>
+
+        <!-- Formulario para borrado de libros -->
+        <div class="contenedorCentrado">
+            <form method="GET" action="">
+                <label> ISBN </label> <br>
+                <input type="text" name="isbn" placeholder="12345678" required>
+                <button type="submit" class="boton" name="borrar" value="1"> Eliminar </button>
+            </form>
+        </div>
+    </div>
 
 
 
+    <?php
+    $options = ['location' => 'http://localhost/entornoServidor/Unidad4/Biblioteca/soap/soap_server.php', 'uri' => 'http://localhost/entornoServidor/Unidad4/Biblioteca/soap/'];
+    $client = new SoapClient(null, $options);
+    // Listar libros
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['consultar'])): ?>
+        <div class='contendoTabla'>
+            <div class="titulosTabla">
+                <div class="col"> Titulo </div>
+                <div class="col"> Autor </div>
+                <div class="col"> Categoria </div>
+                <div class="col"> ISBN </div>
+                <div class="col"> Edición </div>
+                <div class="col"> Promoción </div>
+                <div class="col"> Fecha </div>
+                <div class="col"> Portada </div>
 
-?>
+            </div>
+            <?php
+            $libros = $client->listarLibros();
+            foreach ($libros as $unLibro): ?>
+                <div class="listadoFila">
+                    <div class="col"><?php echo $unLibro['titulo'] ?></div>
+                    <div class="col"><?php echo $unLibro['autor'] ?></div>
+                    <div class="col"><?php echo $unLibro['categoria'] ?></div>
+                    <div class="col"><?php echo $unLibro['isbn'] ?></div>
+                    <div class="col"><?php echo $unLibro['edicion'] ?></div>
+                    <div class="col"><?php echo $unLibro['promocion'] ?></div>
+                    <div class="col"><?php echo $unLibro['fecha'] ?></div>
+                    <div class="col"><?php echo $unLibro['portada'] ?></div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else:
+
+    endif;
+
+    //Borar libros.
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['borrar']) && isset($_GET['isbn'])) {
+        echo "<div class='contenedorCentrado'>" . $client->eliminarLibro($_GET['isbn']) . "</div>";
+    }
+
+    //Creación de un libro
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["titulo"]) && isset($_POST["autor"]) && isset($_POST["categoria"]) && isset($_POST["isbn"]) && isset($_POST["edicion"]) && isset($_POST["promocion"]) && isset($_POST["fecha"])) {
+        $titulo = $_POST['titulo'];
+        $autor = $_POST['autor'];
+        $categoria = $_POST['categoria'];
+        $isbn = $_POST['isbn'];
+        $edicion = $_POST['edicion'];
+        $promocion = $_POST['promocion'];
+        $fecha = $_POST['fecha'];
+
+        echo "<div class='contenedorCentrado'>" . $client->crearLibro($titulo, $autor, $categoria, $isbn, $edicion, $promocion, $fecha) . "</div>";
+
+
+    }
+
+    ?>
+
+</body>
+
+</html>
